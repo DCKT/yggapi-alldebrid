0b99d2345927989b6d038812d8773db533d31032
Thomas Deconinck <thomas.deconinck@colisweb.com>
2025-03-03 19:11:14 +0100

    a bunch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { Button, Card, Text } from "@radix-ui/themes";
+import { Button, Card, Spinner, Text } from "@radix-ui/themes";
 import { useState, useEffect } from "react";
 import { match } from "ts-pattern";
-import { Option } from "@swan-io/boxed";
+import { AsyncData, Option, Result } from "@swan-io/boxed";
+import { Toast } from "radix-ui";
 
 type Torrent = {
   id: number;
@@ -24,25 +25,35 @@ enum DisplayType {
   Grid,
   Row,
 }
+type ToastMessage = {
+  status: "success" | "error";
+  message: string;
+};
 
 export const App = () => {
   const [query, setQuery] = useState("");
-  const [results, setResults] = useState<Option<Torrent[]>>(Option.None());
+  const [results, setResults] = useState<AsyncData<Result<Torrent[], any>>>(
+    AsyncData.NotAsked,
+  );
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const [displayType, setDisplayType] = useState(DisplayType.Grid);
+  const [toastMessage, setToastMessage] = useState<Option<ToastMessage>>(
+    Option.None(),
+  );
 
   const fetchTorrents = async (searchQuery: string, page: number) => {
+    setResults(AsyncData.Loading);
     try {
       const response = await fetch(`/api/search`, {
         method: "POST",
         body: JSON.stringify({ search: searchQuery, page: page }),
       });
       const data = await response.json();
-      setResults(Option.Some(data));
+      setResults(AsyncData.Done(Result.Ok(data)));
       setTotalPages(data.total_pages || 1);
     } catch (error) {
-      // TODO: handle error
+      setResults(AsyncData.Done(Result.Error(error)));
     }
   };
 
@@ -62,19 +73,29 @@ export const App = () => {
         body: JSON.stringify({ torrentId: torrentId }),
       });
       const data: SaveResponse = await response.json();
-      // TODO: handle success message
+
+      if (data.status === "success") {
+        setToastMessage(
+          Option.Some({ status: "success", message: "File saved" }),
+        );
+      }
       // TODO: handle torrent pending
     } catch (error) {
-      // TODO: handle error
+      setToastMessage(
+        Option.Some({ status: "error", message: "Something went wrong" }),
+      );
     }
   };
 
   return (
     <div className="p-4">
-      <div className="flex space-x-2 items-center mb-4">
+      <div className="flex flex-col lg:flex-row gap-4 items-center mb-4">
         <form
           onSubmit={(e) => {
             e.preventDefault();
+            if (results.isLoading()) {
+              return;
+            }
             onSubmit();
           }}
         >
@@ -90,8 +111,8 @@ export const App = () => {
             🔎
           </button>
         </form>
-        {results.isSome() ? (
-          <>
+        {results.isDone() ? (
+          <section className="hidden lg:flex flex-row gap-2">
             <Button
               disabled={displayType === DisplayType.Grid}
               onClick={(_) => {
@@ -108,47 +129,78 @@ export const App = () => {
             >
               colonne
             </Button>
-          </>
+          </section>
         ) : null}
       </div>
       <div
         className={`grid gap-4 ${displayType === DisplayType.Grid ? "grid-cols-1 md:grid-cols-2 lg:grid-cols-3" : "grid-rows-1"}`}
       >
         {results.match({
-          None: () => null,
-          Some: (d) => {
-            return match(d)
-              .with([], () => <Text>No results</Text>)
-              .otherwise(() => {
-                return d.map((torrent) => (
-                  <Card key={torrent.id}>
-                    <h3 className="text-lg font-bold break-all">
-                      {torrent.title}
-                    </h3>
-                    <section className="flex flex-row items-center justify-between">
-                      <div>
-                        <p className="text-sm text-gray-700">
-                          Taille: {octetToMega(torrent.size).toFixed(2)} Mb
-                        </p>
-                        <p className="text-sm text-gray-700">
-                          Seeders: {torrent.seeders}
-                        </p>
-                      </div>
-                      <Button
-                        className="cursor-pointer"
-                        onClick={(_) => {
-                          saveTorrent(torrent.id);
-                        }}
-                      >
-                        💾
-                      </Button>
-                    </section>
-                  </Card>
-                ));
-              });
+          NotAsked: () => null,
+          Loading: () => <Spinner />,
+          Done: (d) => {
+            return d.match({
+              Error: (error) => (
+                <section className="flex flex-col gap-4 text-red-700">
+                  <Text>Une erreur est survenue</Text>{" "}
+                  {error ? <Text>{error}</Text> : null}
+                </section>
+              ),
+              Ok: (d) => {
+                return match(d)
+                  .with([], () => <Text>No results</Text>)
+                  .otherwise(() => {
+                    return d.map((torrent) => (
+                      <Card key={torrent.id}>
+                        <h3 className="text-sm lg:text-lg font-bold break-all">
+                          {torrent.title}
+                        </h3>
+                        <section className="flex flex-row items-center justify-between">
+                          <div>
+                            <p className="text-xs lg:text-sm text-gray-700">
+                              Taille: {octetToMega(torrent.size).toFixed(2)} Mb
+                            </p>
+                            <p className="text-sm text-gray-700">
+                              Seeders: {torrent.seeders}
+                            </p>
+                          </div>
+                          <Button
+                            className="cursor-pointer"
+                            onClick={(_) => {
+                              saveTorrent(torrent.id);
+                            }}
+                          >
+                            💾
+                          </Button>
+                        </section>
+                      </Card>
+                    ));
+                  });
+              },
+            });
           },
         })}
       </div>
+      {toastMessage.match({
+        None: () => null,
+        Some: ({ status, message }) => {
+          return (
+            <Toast.Provider swipeDirection="right">
+              <Toast.Root
+                className={`ToastRoot ${status === "success" ? "!bg-green-100" : "!bg-red-100"}`}
+              >
+                <Toast.Title
+                  className={`ToastTitle ${status === "success" ? "!text-green-700" : "!text-red-700"}`}
+                >
+                  {message}
+                </Toast.Title>
+              </Toast.Root>
+
+              <Toast.Viewport className="ToastViewport" />
+            </Toast.Provider>
+          );
+        },
+      })}
     </div>
   );
 };
