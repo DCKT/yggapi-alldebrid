28a41f34e72f4abf34bf93f52be68fb156f3dc41
Thomas Deconinck <thomas.deconinck@colisweb.com>
2025-03-03 16:49:44 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
+import { Button, Card, Text } from "@radix-ui/themes";
 import { useState, useEffect } from "react";
+import { match } from "ts-pattern";
+import { Option } from "@swan-io/boxed";
 
 type Torrent = {
   id: number;
@@ -17,11 +20,17 @@ type SaveResponse = {
   status: "success" | "pending";
 };
 
+enum DisplayType {
+  Grid,
+  Row,
+}
+
 export const App = () => {
   const [query, setQuery] = useState("");
-  const [results, setResults] = useState<Torrent[]>([]);
+  const [results, setResults] = useState<Option<Torrent[]>>(Option.None());
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [displayType, setDisplayType] = useState(DisplayType.Grid);
 
   const fetchTorrents = async (searchQuery: string, page: number) => {
     try {
@@ -30,7 +39,7 @@ export const App = () => {
         body: JSON.stringify({ search: searchQuery, page: page }),
       });
       const data = await response.json();
-      setResults(data);
+      setResults(Option.Some(data));
       setTotalPages(data.total_pages || 1);
     } catch (error) {
       // TODO: handle error
@@ -62,7 +71,7 @@ export const App = () => {
 
   return (
     <div className="p-4">
-      <div className="flex space-x-2 mb-4">
+      <div className="flex space-x-2 items-center mb-4">
         <form
           onSubmit={(e) => {
             e.preventDefault();
@@ -81,33 +90,64 @@ export const App = () => {
             🔎
           </button>
         </form>
+        {results.isSome() ? (
+          <>
+            <Button
+              disabled={displayType === DisplayType.Grid}
+              onClick={(_) => {
+                setDisplayType((_) => DisplayType.Grid);
+              }}
+            >
+              grille
+            </Button>
+            <Button
+              disabled={displayType === DisplayType.Row}
+              onClick={(_) => {
+                setDisplayType((_) => DisplayType.Row);
+              }}
+            >
+              colonne
+            </Button>
+          </>
+        ) : null}
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {results.map((torrent) => (
-          <div key={torrent.id}>
-            <div className="p-4 border border-gray-200 rounded-lg">
-              <h3 className="text-lg font-bold break-all">{torrent.title}</h3>
-              <section className="flex flex-row items-center justify-between">
-                <div>
-                  <p className="text-sm text-gray-700">
-                    Taille: {octetToMega(torrent.size).toFixed(2)} Mb
-                  </p>
-                  <p className="text-sm text-gray-700">
-                    Seeders: {torrent.seeders}
-                  </p>
-                </div>
-                <button
-                  className="bg-blue-100 p-2 rounded cursor-pointer hover:bg-blue-200"
-                  onClick={(_) => {
-                    saveTorrent(torrent.id);
-                  }}
-                >
-                  💾
-                </button>
-              </section>
-            </div>
-          </div>
-        ))}
+      <div
+        className={`grid gap-4 ${displayType === DisplayType.Grid ? "grid-cols-1 md:grid-cols-2 lg:grid-cols-3" : "grid-rows-1"}`}
+      >
+        {results.match({
+          None: () => null,
+          Some: (d) => {
+            return match(d)
+              .with([], () => <Text>No results</Text>)
+              .otherwise(() => {
+                return d.map((torrent) => (
+                  <Card key={torrent.id}>
+                    <h3 className="text-lg font-bold break-all">
+                      {torrent.title}
+                    </h3>
+                    <section className="flex flex-row items-center justify-between">
+                      <div>
+                        <p className="text-sm text-gray-700">
+                          Taille: {octetToMega(torrent.size).toFixed(2)} Mb
+                        </p>
+                        <p className="text-sm text-gray-700">
+                          Seeders: {torrent.seeders}
+                        </p>
+                      </div>
+                      <Button
+                        className="cursor-pointer"
+                        onClick={(_) => {
+                          saveTorrent(torrent.id);
+                        }}
+                      >
+                        💾
+                      </Button>
+                    </section>
+                  </Card>
+                ));
+              });
+          },
+        })}
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import "./styles.css";
+import "@radix-ui/themes/styles.css";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
+import { Theme } from "@radix-ui/themes";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = createRoot(document.getElementById("root") as HTMLElement);
-  root.render(<App />);
+  root.render(
+    <Theme>
+      <App />
+    </Theme>,
+  );
 });
